80261d79e94514945d612568456ad1f0ebead865
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-25 07:42:24 -0700

    add king to board

--- src/models/game.js
+++ src/models/game.js
@@ -3,16 +3,17 @@ const Pawn = require("./pieces/pawn");
 const Rook = require("./pieces/rook");
 const Bishop = require("./pieces/bishop");
 const Knight = require("./pieces/knight");
+const King = require("./pieces/king");
 
 class Game {
 	constructor() {
 		this.board = this.buildInitialBoard();
-		// this.capturedPieces = {
-		// 	black: [],
-		// 	white: []
-		// };
-		// this.player1 = {};
-		// this.player2 = {};
+		this.capturedPieces = {
+			black: [],
+			white: []
+		};
+		this.player1 = {};
+		this.player2 = {};
 	}
 
 	buildInitialBoard() {
@@ -22,7 +23,7 @@ class Game {
 		board[0][1] = new Space(0, 1, new Knight(true, { x: 1, y: 0 }));
 		board[0][2] = new Space(0, 2, new Bishop(true, { x: 2, y: 0 }));
 		board[0][3] = new Space(0, 3);
-		board[0][4] = new Space(0, 4);
+		board[0][4] = new Space(0, 4, new King(true, { x: 4, y: 0 }));
 		board[0][5] = new Space(0, 5, new Bishop(true, { x: 5, y: 0 }));
 		board[0][6] = new Space(0, 6, new Knight(true, { x: 0, y: 6 }));
 		board[0][7] = new Space(0, 7, new Rook(true, { x: 7, y: 0 }));
@@ -47,7 +48,7 @@ class Game {
 		board[7][0] = new Space(7, 0, new Rook(false, { x: 0, y: 7 }));
 		board[7][1] = new Space(7, 1, new Knight(false, { x: 1, y: 7 }));
 		board[7][2] = new Space(7, 2, new Bishop(false, { x: 2, y: 7 }));
-		board[7][3] = new Space(7, 3);
+		board[7][3] = new Space(7, 3, new King(false, { x: 3, y: 7 }));
 		board[7][4] = new Space(7, 4);
 		board[7][5] = new Space(7, 5, new Bishop(false, { x: 5, y: 7 }));
 		board[7][6] = new Space(7, 6, new Knight(false, { x: 6, y: 7 }));
@@ -59,6 +60,6 @@ class Game {
 
 const { board } = new Game();
 
-console.log(board[7][6].piece);
+console.log(board[7][3].piece.getPossibleMoves(board));
 
 module.exports = Game;
--- src/models/pieces/king.js
+++ src/models/pieces/king.js
@@ -1,15 +1,47 @@
 const Piece = require("../piece");
+const getSpaceIds = require("../../helpers/getSpaceIds");
 
 class King extends Piece {
 	constructor(isWhite, position) {
 		super(isWhite, position, "♚ ", "♔ ");
 		this._id = isWhite ? "K" : "k";
+		this.inCheck = false;
 		this.hasCastled = false;
+		this.isFirstMove = true;
 	}
 
-	getPossibleMoves() {
+	getPossibleMoves(board) {
 		let possibleMoves = [];
 		const { x, y } = this.position;
+
+		// all possible king moves, in clockwise order
+		const kingMoves = [
+			{ x, y: y + 1 },
+			{ x: x + 1, y: y + 1 },
+			{ x: x + 1, y },
+			{ x: x + 1, y: y - 1 },
+			{ x, y: y - 1 },
+			{ x: x - 1, y: y - 1 },
+			{ x: x - 1, y },
+			{ x: x - 1, y: y + 1 }
+		];
+
+		kingMoves.forEach(move => {
+			// if move is off the board, skip it
+			if (move.x > 7 || move.x < 0 || move.y > 7 || move.y < 0) return;
+
+			const { piece } = board[move.y][move.x];
+			if (piece !== null) {
+				// if space is occupied by enemy, add space
+				if (piece.isWhite !== this.isWhite) {
+					possibleMoves.push(move);
+				}
+			} else {
+				possibleMoves.push(move);
+			}
+		});
+
+		return getSpaceIds(possibleMoves);
 	}
 }
 
