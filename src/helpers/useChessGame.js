dcdaa6c8f934f4a6f6af27c0f958f9349b7177fb
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-26 19:56:59 -0700

    add rerender listeners to hook

--- src/helpers/useChessGame.js
+++ src/helpers/useChessGame.js
@@ -4,7 +4,11 @@ const Game = require("../models/game");
 const useChessGame = () => {
 	const [game] = React.useState(new Game());
 
-	React.useEffect(() => {}, [game.history]);
+	React.useEffect(() => {}, [
+		game.history,
+		game.player2.name,
+		game.player1.name
+	]);
 
 	return game;
 };
