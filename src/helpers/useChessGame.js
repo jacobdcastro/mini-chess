bd80761f6f0a8319695b8197d486da9b88b8beb5
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-30 01:05:01 -0700

    test and comment out new code in hook

--- src/helpers/useChessGame.js
+++ src/helpers/useChessGame.js
@@ -1,16 +1,29 @@
 const React = require("react");
 const Game = require("../models/game");
 
+// const data = new Game();
+
 const useChessGame = () => {
-	const [game] = React.useState(new Game());
+	const [game, setGame] = React.useState(new Game());
 
-	React.useEffect(() => {}, [
-		game.history,
-		game.player2.name,
-		game.player1.name
-	]);
+	// React.useEffect(() => {
+	// 	setGame({ ...game, started: new Date() });
+	// 	console.log(game);
+	// }, []);
 
-	return game;
+	// const [players, setPlayers] = React.useState({
+	// 	player1: game.player1,
+	// 	player2: game.player2,
+	// 	turn: game.turn
+	// });
+
+	// const [board, setBoard] = React.useState({
+	// 	player1: game.player1,
+	// 	player2: game.player2,
+	// 	turn: game.turn
+	// });
+
+	return { game, setGame };
 };
 
 module.exports = useChessGame;
