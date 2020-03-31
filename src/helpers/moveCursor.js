959d4a279508ab85f5cffa43f565bb02f7c7d811
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-31 12:15:57 -0700

    fix: incorrect conditions check

--- src/helpers/moveCursor.js
+++ src/helpers/moveCursor.js
@@ -1,20 +1,20 @@
 const moveCursor = (input, key, player) => {
 	const { x, y } = player.cursorPosition;
 
-	if (key.upArrow || (input === "w" && y < 8)) {
+	if ((key.upArrow || input === "w") && y < 7) {
 		return { x, y: y + 1 };
 	}
-	if (key.rightArrow || (input === "d" && x < 8)) {
+	if ((key.rightArrow || input === "d") && x < 7) {
 		return { x: x + 1, y };
 	}
-	if (key.downArrow || (input === "s" && y > 0)) {
+	if ((key.downArrow || input === "s") && y > 0) {
 		return { x, y: y - 1 };
 	}
-	if (key.leftArrow || (input === "a" && x > 0)) {
+	if ((key.leftArrow || input === "a") && x > 0) {
 		return { x: x - 1, y };
 	}
 
-	return;
+	return { x, y };
 };
 
 module.exports = moveCursor;
