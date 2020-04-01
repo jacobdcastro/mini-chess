dc0c92578ef13c29ba5fd6d7b0aaab046431c889
Jacob D. Castro <jdcastro.business@gmail.com>
2020-03-31 21:57:01 -0700

    add reverse black-side cursor movement

--- src/helpers/cursorActions.js
+++ src/helpers/cursorActions.js
@@ -4,14 +4,16 @@ const moveCursorOnPossibleMoves = require("./moveCursorOnPossibleMoves");
 const moveCursor = (input, key, player) => {
 	const { x, y } = player.cursorPosition;
 
-	if ((key.upArrow || input === "w") && y < 7) {
-		return { x, y: y + 1 };
+	if (key.upArrow || input === "w") {
+		if (player.color === "w" && y < 7) return { x, y: y + 1 };
+		else if (y > 0) return { x, y: y - 1 };
 	}
 	if ((key.rightArrow || input === "d") && x < 7) {
 		return { x: x + 1, y };
 	}
-	if ((key.downArrow || input === "s") && y > 0) {
-		return { x, y: y - 1 };
+	if (key.downArrow || input === "s") {
+		if (player.color === "w" && y > 0) return { x, y: y - 1 };
+		else if (y < 7) return { x, y: y + 1 };
 	}
 	if ((key.leftArrow || input === "a") && x > 0) {
 		return { x: x - 1, y };
