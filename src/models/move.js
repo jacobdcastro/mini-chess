5b38f28d5b9654d2a23def1b0723effe2d71afee
Jacob D. Castro <jdcastro.business@gmail.com>
2020-04-01 18:13:09 -0700

    add move class for moving pieces

--- src/models/move.js
+++ src/models/move.js
@@ -1,1 +1,21 @@
-const move = piece => {};
+const getSpaceId = require("../helpers/getSpaceId");
+
+class Move {
+	constructor(board, piece, newCoords) {
+		this.movedPieceIsWhite = piece.isWhite;
+		this.movedPieceId = piece._id;
+		this.startingLocation = piece.position;
+		this.startingLocation._id = getSpaceId(piece.position.y, piece.position.x);
+		this.endingLocation = newCoords;
+		this.endingLocation._id = getSpaceId(newCoords.y, newCoords.x);
+		this.capturedPieceId = this.getCapturedPiece(board);
+	}
+
+	getCapturedPiece(board) {
+		const { x, y } = this.endingLocation;
+		const { piece } = board[y][x];
+		return piece ? piece._id : null;
+	}
+}
+
+module.exports = Move;
