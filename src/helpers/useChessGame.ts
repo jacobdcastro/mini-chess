892090131617b41546739a0ab3fde9c050954728
Jacob D. Castro <jdcastro.business@gmail.com>
2020-04-05 12:36:15 -0700

    prepare to convert to useReducer

--- src/helpers/useChessGame.ts
+++ src/helpers/useChessGame.ts
@@ -2,28 +2,9 @@
 import React from 'react';
 import Game from '../models/game';
 
-// const data = new Game();
-
+// TODO convert to useReducer
 const useChessGame = () => {
   const [game, setGame] = React.useState(new Game());
-
-  // React.useEffect(() => {
-  // 	setGame({ ...game, started: new Date() });
-  // 	console.log(game);
-  // }, []);
-
-  // const [players, setPlayers] = React.useState({
-  // 	player1: game.player1,
-  // 	player2: game.player2,
-  // 	turn: game.turn
-  // });
-
-  // const [board, setBoard] = React.useState({
-  // 	player1: game.player1,
-  // 	player2: game.player2,
-  // 	turn: game.turn
-  // });
-
   return { game, setGame };
 };
 
