b8a7e1bb677a46956564e635dd792eae677b0768
Jacob D. Castro <jdcastro.business@gmail.com>
2020-04-05 14:47:11 -0700

    add interfaces and types to be used across project

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -1,16 +1,20 @@
-// @ts-nocheck
-import Piece from '../models/piece';
 import Space from '../models/space';
+import Pawn from '../models/pieces/pawn';
+import Bishop from '../models/pieces/bishop';
+import Knight from '../models/pieces/knight';
+import Rook from '../models/pieces/rook';
+import King from '../models/pieces/king';
+import Queen from '../models/pieces/queen';
 
 export interface Position {
-  x: Number;
-  y: Number;
-  _id?: String;
+  x: number;
+  y: number;
+  _id?: string;
 }
 
 export interface PieceRef {
-  readonly type: String;
-  readonly _id: String;
+  readonly type: string;
+  readonly _id: string;
 }
 
 export type PieceIcon =
@@ -30,3 +34,14 @@ export type PieceIcon =
 export type PieceValue = 1 | 3 | 5 | 9 | 1000000000000;
 
 export type Board = Space[][];
+
+export type Color = 'w' | 'b';
+
+export type AnyPiece = Pawn | Bishop | Knight | Rook | King | Queen;
+
+export interface AllPieces {
+  white: AnyPiece[];
+  black: AnyPiece[];
+}
+
+export interface HistoryItem {}
